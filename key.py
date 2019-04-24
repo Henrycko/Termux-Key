e111386aa3733f63e9611fb190a9485d822b9faa
Henrycko <49187820+Henrycko@users.noreply.github.com>
2019-04-24 14:44:13 +0700

    Create key.py

---
+++ key.py
@@ -0,0 +1,36 @@
+
+import os
+from time import sleep
+
+a ='\033[92m'
+b ='\033[91m'
+c ='\033[0m'
+os.system('clear')
+print(a+'\t  Shortcut for help you')
+print(a+'+'*40)
+print('\nProses..')
+sleep(1)
+print(b+'\n[-] Making TERMUX Properties Directory...
+sleep(1)
+try:
+      os.mkdir('/data/data/com.termux/files/home/.termux')
+except:
+      pass
+print(a+'[+]Success !')
+sleep(1)
+print(b+'\n[-] Making Setup File...')
+sleep(1)
+
+key = "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]"
+kontol = open('/data/data/com.termux/files/home/.termux/termux.properties','w')
+kontol.write(key)
+kontol.close()
+sleep(1)
+print(a+'[+] Success !')
+sleep(1)
+print(b+'\n[-] Setting up...')
+sleep(2)
+os.system('termux-reload-settings')
+print(a+'[+] Successfully ! ^^'+c+'\n\nThank you for installing :)
+
+
